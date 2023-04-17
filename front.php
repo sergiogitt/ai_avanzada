cb6ce3e7c42f00e553a5bead36523b92170a32f3
sergiogitt <sromero147@gmail.com>
2023-04-17 15:05:56 +0200

    Considered interactions on the click of the button

--- front.php
+++ front.php
@@ -10,34 +10,37 @@
 <body>
     
         <textarea id="prompt" oninput="test()"></textarea>
-        <button>Submit</button>
+        <button onclick="new_interaction()">Submit</button>
        
     <script>
         let first_input=true;
         let id_prompt=null;
+        let interation=0;
+        function new_interaction(){
+            interation++;
+            document.getElementById("prompt").value=""; 
+            first_input=true;
+        }
         function test(content) {
-            var input = document.getElementById("prompt").value;
-           
+            var input = document.getElementById("prompt").value;          
             if(first_input){
                 console.log("Inserting "+document.getElementById('prompt').value);
                 $.ajax({
-                        type : "POST",  //type of method
-                        url  : "index.php",  //your page
-                        data : { prompt : input ,attempt:true,customer_id:1,iterations:1},// passing the values
+                        type : "POST",  
+                        url  : "index.php",  
+                        data : { prompt : input ,attempt:true,customer_id:1,iterations:interation},
                         success: function(res){  
                             console.log("bien"+ res)
                             id_prompt=res;
                         }
                     });
                 first_input=false;
-
             }else{
-                //console.log("Updating "+document.getElementById('prompt').value);
                 console.log(id_prompt)
                 $.ajax({
-                        type : "POST",  //type of method
-                        url  : "index.php",  //your page
-                        data : { prompt : input,prompt_id:id_prompt,customer_id:1,iterations:1},// passing the values
+                        type : "POST", 
+                        url  : "index.php",  
+                        data : { prompt : input,prompt_id:id_prompt,customer_id:1,iterations:interation},
                         success: function(res){  
                             console.log("updating "+ res)
                         }
