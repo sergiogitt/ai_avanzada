79ecd575f17b4a9dac2b551f2d8c0bc02dadd30b
sergiogitt <sromero147@gmail.com>
2023-04-20 13:23:41 +0200

    Api respone shown at the app

--- index.php
+++ index.php
@@ -5,16 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body onload="get_actual_data_user(1)">
-    <div id="block1">
-        <textarea id="prompt1" oninput="inserting_into_db(1)" rows='20' cols='70'></textarea>
-        <button onclick="send_ai(1)">Send To AI</button>
-        <button onclick="new_interaction()">New iteration +</button>
-        <p id="action1"></p>
+<div id="flex_container">
+    <div id="blocks_container">
+        <div id="block1">
+            <textarea id="prompt1" oninput="inserting_into_db(1)" rows='20' cols='70'></textarea>
+            <button onclick="send_ai(1)">Send To AI</button>
+            <button onclick="new_interaction()">New iteration +</button>
+            <p id="action1"></p>
+        </div>
     </div>
-    
+   
+    <div id="response">
+        <h3>AI response</h3>
+    </div>
+</div>
 </body>
 <script>
         let number_actual_block=1;
@@ -25,51 +33,52 @@
         control_insertion_prompts[1]=true;
         let ini=0;
         let getting_info=false;
-function get_actual_data_user(user_id){
-    $.ajax({
-            type : "POST",  
-            url  : "back.php",  
-            data : { customer_id:user_id,getFirstData:true},
-            success: function(res){   
-                let answer=JSON.parse(res);
-                if(answer.not_found!="Not found that client"){
-                    control_insertion_prompts[1]=false;
-                    getting_info=true;
-                    ids_prompts[1]=answer.PROMPT_ID;
-                    document.getElementById("prompt1").value=answer.prompt;
-                    $.ajax({
+        function get_actual_data_user(user_id){
+            $.ajax({
                     type : "POST",  
                     url  : "back.php",  
-                    data : { customer_id:user_id,getSecondData:true,prompt_id:ids_prompts[1]},
-                    success: function(res2){  
-                        let data=JSON.parse(res2);
-                        data.forEach(element => {
-                            create_new_prompts_inputs(true,element);
-                            //Adding new element to the array which contorls the insertions or updates of the prompts
-                            control_insertion_prompts[number_actual_block]=false;
-                        });
-                        
-                        
+                    data : { customer_id:user_id,getFirstData:true},
+                    success: function(res){   
+                        let answer=JSON.parse(res);
+                        if(answer.not_found!="Not found that client"){
+                            control_insertion_prompts[1]=false;
+                            getting_info=true;
+                            ids_prompts[1]=answer.PROMPT_ID;
+                            document.getElementById("prompt1").value=answer.prompt;
+                            $.ajax({
+                            type : "POST",  
+                            url  : "back.php",  
+                            data : { customer_id:user_id,getSecondData:true,prompt_id:ids_prompts[1]},
+                            success: function(res2){  
+                                let data=JSON.parse(res2);
+                                data.forEach(element => {
+                                    create_new_prompts_inputs(true,element);
+                                    //Adding new element to the array which contorls the insertions or updates of the prompts
+                                    control_insertion_prompts[number_actual_block]=false;
+                                });
+                                
+                                
+                                }
+                            });
                         }
-                    });
-                }
-               
-            }
-        });
-    
-}
+                    
+                    }
+                });
+            
+        }
         function send_ai(number){
             //ID of elements
-            let prompt="prompt"+number;
+            let prompt1="prompt"+number;
             let action="action"+number;
             //Control if there is something on the textarea
-            if(document.getElementById(prompt).value!=""){
+            if(document.getElementById(prompt1).value!=""){
                 const REACT_APP_OPENAI_API_KEY="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";
                // Define the data variable with prompt and API parameters
                const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
 
                 // Parámetros de la solicitud
-                const prompt = "Dime un chiste";
+                let prompt =document.getElementById(prompt1).value ;
+                console.log(prompt)
                 const max_tokens = 5;
                 const n = 1;
 
@@ -83,7 +92,7 @@ function get_actual_data_user(user_id){
                 body: JSON.stringify({
                     
                     "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "user", "content": "Let me know if the api is working"}],
+                    "messages": [{"role": "user", "content":prompt}],
                     "temperature": 0.7
                 })
                 };
@@ -91,7 +100,15 @@ function get_actual_data_user(user_id){
                 // Hacer la solicitud a la API
                 fetch(API_ENDPOINT, options)
                 .then(response => response.json())
-                .then(data => console.log(data))
+                .then(data =>{ 
+                    let content=data.choices[0].message.content;
+                    console.log(content)
+                    var newP = document.createElement("p");
+                    newP.setAttribute("id", "responseP");
+                    newP.innerHTML=content;
+                   
+                    document.getElementById("response").appendChild(newP);
+                })
                 .catch(error => console.error(error));
 
             }else{
@@ -132,7 +149,8 @@ function get_actual_data_user(user_id){
                 newDiv.appendChild(newIterationButton);
                 newDiv.appendChild(newP);
                 //Adding de new div to the body
-                document.body.appendChild(newDiv);
+                let container=document.getElementById("blocks_container");
+                container.appendChild(newDiv);
                 //Updating new values of block
                 number_actual_block++;
                 number_new_block++;
