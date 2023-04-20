9fbf31f48bba62d42089d63a1711c5ac03cfa46f
sergiogitt <sromero147@gmail.com>
2023-04-20 15:22:37 +0200

    Bootstrapped and added some bootstrap buttons

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body onload="get_actual_data_user(1)">
@@ -13,14 +17,15 @@
     <div id="blocks_container">
         <div id="block1">
             <textarea id="prompt1" oninput="inserting_into_db(1)" rows='20' cols='70'></textarea>
-            <button onclick="send_ai(1)">Send To AI</button>
-            <button onclick="new_interaction()">New iteration +</button>
+            <button type="submit" class="btn btn-primary" onclick="send_ai(1)">Send To AI</button>
+            <button type="submit" class="btn btn-primary" onclick="new_interaction()">New iteration +</button>
             <p id="action1"></p>
         </div>
     </div>
    
     <div id="response">
         <h3>AI response</h3>
+        <p id="response_ai"></p>
     </div>
 </div>
 </body>
@@ -103,11 +108,10 @@
                 .then(data =>{ 
                     let content=data.choices[0].message.content;
                     console.log(content)
-                    var newP = document.createElement("p");
-                    newP.setAttribute("id", "responseP");
+                    var newP = document.getElementById("response_ai");
                     newP.innerHTML=content;
                    
-                    document.getElementById("response").appendChild(newP);
+                    document.c("response").appendChild(newP);
                 })
                 .catch(error => console.error(error));
 
@@ -134,10 +138,14 @@
                 var newButton = document.createElement("button");
                 newButton.innerHTML = "Send to AI";
                 newButton.setAttribute("onclick",`send_ai('${number_new_block}')`);
+                newButton.setAttribute("type",`submit`);
+                newButton.setAttribute("class",`btn btn-primary`);
                 
                 var newIterationButton = document.createElement("button");
                 newIterationButton.innerHTML = "New iteration +";
                 newIterationButton.setAttribute("onclick",`new_interaction('${number_new_block}')`);
+                newIterationButton.setAttribute("type",`submit`);
+                newIterationButton.setAttribute("class",`btn btn-primary`);
 
                 var newP = document.createElement("p");
                 newP.setAttribute("id", "action"+number_new_block);
